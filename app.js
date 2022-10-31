97c42f2a351d28d4fc0ce023edc188384b9b44ef
Christopher Stone <cmstone@student.fullsail.edu>
2022-10-31 18:49:20 -0400

    Fix function and tests to accept correct inputs

--- app.js
+++ app.js
@@ -1,10 +1,6 @@
 const obscure = (card) => {
-    console.log(card.length)
-    if(card.length == 14) {
-        return `xxxxxxxx${card.slice(-4)}`
-    }
-    else if(card.length == 16) {
-        return `xxxxxxxxxxxx${card.slice(-4)}`
+    if(card.length >= 12 && card.length <= 16) {
+        return card.slice(-4).padStart(card.length, "*")
     }
     else {
         return `Invalid credit card length, please try again!`
@@ -12,6 +8,3 @@ const obscure = (card) => {
 }
 
 module.exports = obscure
-
-// card.length != 14 || card.length != 16
-
--- app.test.js
+++ app.test.js
@@ -1,18 +1,14 @@
 const card = require("./app");
 
 describe('Credit Card Obscure Tests', () => {
-    test('If a credit card number is not 14 or 16 characters in length it should report a error message', () => {
-        expect(card("25364568985")).toEqual("Invalid credit card length, please try again!")
-        expect(card("25364568985")).not.toEqual("xxxxxxx8985")
+    test('If a credit card number is not 12 - 16 characters in length it should report a error message', () => {
+        expect(card("25364568")).toEqual("Invalid credit card length, please try again!")
+        expect(card("25364568")).not.toEqual("****4568")
     })
 
-    test('If a credit card number is 14 characters in length it should obscure the first 8 numbers and show the last 4', () => {
-        expect(card("12348569456724")).toEqual("xxxxxxxx6724")
+    test('If a credit card number is between 12-16 characters in length it should obscure the first 8 numbers and show the last 4', () => {
+        expect(card("12348569456724")).toEqual("**********6724")
+        expect(card("156482566238942")).toEqual("***********8942")
         expect(card("12348569456724")).not.toEqual("Invalid credit card length, please try again!")
-    })
-
-    test('If a credit card number is 16 characters in length it should obscure the first 8 numbers and show the last 4', () => {
-        expect(card("1234856945679457")).toEqual("xxxxxxxxxxxx9457")
-        expect(card("1234856945679457")).not.toEqual("Invalid credit card length, please try again!")
     })
 })
